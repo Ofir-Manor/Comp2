b89dfe6bea89db3f57430566a8ab412d458fa14d
Ofir-Manor <ofir.manor@technion.ac.il>
2021-04-21 13:01:02 +0300

    first upload

---
+++ output.h
@@ -0,0 +1,12 @@
+#ifndef _236360_2_
+#define _236360_2_
+#include
+using namespace std;
+namespace output
+{
+    extern const string rules[];
+    void printProductionRule(int ruleno);
+    void errorLex(int lineno);
+    void errorSyn(int lineno);
+};                 \
+#endif
